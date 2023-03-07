81c963d75ee77738f150712317f473d59d76fd7b
essam <essamheshmey>
2023-03-07 18:11:23 +0200

    عمل تعديل بسيط علي خلفيت مستقبل النص وربط عناصر الوجهة بلكود

--- app/src/main/java/com/essamheshmey/simplewebbrowser/MainActivity.kt
+++ app/src/main/java/com/essamheshmey/simplewebbrowser/MainActivity.kt
@@ -2,8 +2,17 @@ package com.essamheshmey.simplewebbrowser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.widget.EditText
+import android.widget.ImageButton
+
 
 class MainActivity : AppCompatActivity() {
+    val url:EditText=findViewById(R.id.url)
+    val return_btn:ImageButton=findViewById(R.id.return_btn)
+    val search_btn:ImageButton=findViewById(R.id.search_btn)
+    val webview:WebView=findViewById(R.id.webview)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
