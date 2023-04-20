eebbedbc2a73ea28fd2d788f9c5bb52259b10d53
essam <essamheshmey>
2023-04-21 00:26:13 +0200

    Update GitHub account link
    add  buildFeatures {
                viewBinding = true
            }
            in build.gradle

--- app/src/main/java/com/essamheshmey/simplewebbrowser/MainActivity.kt
+++ app/src/main/java/com/essamheshmey/simplewebbrowser/MainActivity.kt
@@ -8,33 +8,37 @@ import android.webkit.URLUtil
 import android.webkit.WebView
 import android.widget.EditText
 import android.widget.ImageButton
+import com.essamheshmey.simplewebbrowser.databinding.ActivityMainBinding
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
+ lateinit var binding: ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val url_text:EditText=findViewById(R.id.url_text)
-        val return_btn:ImageButton=findViewById(R.id.return_btn)
-        val search_btn:ImageButton=findViewById(R.id.search_btn)
-        val webview:WebView=findViewById(R.id.webview)
-        webview.loadUrl("https://github.com/essamhashemy")
-        webview.settings.javaScriptEnabled = true
-        webview.canGoBack()
-        webview.webViewClient = WebClient(this)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+       // val url_text:EditText=findViewById(R.id.url_text)
+        //val return_btn:ImageButton=findViewById(R.id.return_btn)
+      //  val search_btn:ImageButton=findViewById(R.id.search_btn)
+      //  val webview:WebView=findViewById(R.id.webview)
 
-        search_btn.setOnClickListener {
+       binding.webview.loadUrl("https://github.com/essamheshmy")
+        binding.webview.settings.javaScriptEnabled = true
+        binding.webview.canGoBack()
+        binding.webview.webViewClient = WebClient(this)
+
+        binding.searchBtn.setOnClickListener {
             //GlobalScope.launch {
-            val URL =url_text.text.toString()
-           if (URLUtil.isValidUrl(URL)){ webview.loadUrl(URL)}
-          else{ webview.loadUrl("https://www.google.com/search?q=${URL}")}}
+            val URL =binding.urlText.text.toString()
+           if (URLUtil.isValidUrl(URL)){ binding.webview.loadUrl(URL)}
+          else{ binding.webview.loadUrl("https://www.google.com/search?q=${URL}")}}
      //   }
 
-        return_btn.setOnClickListener {webview.goBack()}
+       binding.returnBtn.setOnClickListener {binding.webview.goBack()}
     }
 }
